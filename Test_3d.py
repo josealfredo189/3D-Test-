4ba0a2daad9a1dba945cba5176d40c88073802ff
josealfredo189 <76010675+josealfredo189@users.noreply.github.com>
2021-01-27 17:43:57 -0500

    Update Test_3d.py

--- Test_3d.py
+++ Test_3d.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 #import functions tools3D
 import tools3d as tools
 
-#---Coordenadas
+#---Coordenadas generales
 xg=[]
 yg=[]
 zg=[]
@@ -23,6 +23,7 @@ for i in range(len(x)):
     yg.append(y[i] + yc)
     zg.append(z[i] + zc)
 
+#Plotea las lineas del triangulo
 def plotPlaneLine(xg,yg,zg):
     plt.axis([50,250,150,0])
     plt.grid()
@@ -40,53 +41,59 @@ def plotPlaneLine(xg,yg,zg):
     plt.show()
 
 def hitPoint(x,y,z):
-    #Distance point(0,1)
+    #Distancia del punto 0 al 1
     a = x[1]-x[0]
     b = y[1]-y[0]
     c = z[1]-z[0]
     Q01 = np.sqrt(a*a+b*b+c*c)
 
-    #Distancia del punto(1,2)
+    #Distancia del punto 1 al 2
     a = x[2]-x[1]
     b = y[2]-y[1]
     c = z[2]-z[1]
     Q12 = np.sqrt(a*a+b*b+c*c)
    
-    #Distancia del punto(0,2)
+    #Distancia del punto 0 al 2
     a = x[2]-x[0]
     b = y[2]-y[0]
     c = z[2]-z[0]
     Q02 = np.sqrt(a*a+b*b+c*c)
     
-    #Third point of the triangle
-    #Distancia del punto(1,3)
+    #Distancia del punto 1 al 3
     a = x[3]-x[1]
     b = y[3]-y[1]
     c = z[3]-z[1]
     Q13 = np.sqrt(a*a+b*b+c*c)
-    #Distance point(2,3)
+
+    #Distancia del punto 2 al 3
     a = x[2]-x[3]
     b = y[2]-y[3]
     c = z[2]-z[3]
     Q23 = np.sqrt(a*a+b*b+c*c)
-    #Distance point(0,3)
+
+    #Distancia del punto 0 al 3
     a = x[0]-x[3]
     b = y[0]-y[3]
     c = z[0]-z[3]
     Q03 = np.sqrt(a*a+b*b+c*c)
-    #Area triangle A
+
+    #Calcula el area del triangulo A
+    #con la formula s=(a+b+c)/2     A=raiz(s(s-a)(s-b)(s-c))
     s = (Q01+Q12+Q02)/2
     A = np.sqrt(s * (s-Q01) * (s-Q12) * (s*Q02))
-    #Area triangle A1
+
+    #Calcula el area del triangulo A1
     s1 = (Q01 + Q03 + Q13) /2
     A1 = np.sqrt(s1*(s1-Q01) * (s1-Q03) * (s1-Q13))
-    #Area triangle A2
+
+    #Calcula el area del triangulo A2
     s2 = (Q02 + Q23 + Q03) /2
     A2 = np.sqrt(s2 * (s2-Q02) * (s2-Q23) * (s1-Q03))
 
+    #Regresa el area de los triangulos
     return A,A1,A2
     
-
+#Plotea el triangulo y manda mensaje 
 def plotSquareLinex(xc,yc,zc):
     [A,A1,A2]=hitPoint(x,y,z)
     print('A=',A)
@@ -94,30 +101,42 @@ def plotSquareLinex(xc,yc,zc):
     print('A2=',A2)
     print ('A1+A2',(A1+A2))
     if((A1+A2)>A):
-        plt.text(170,60,'El HIT PONT esta fuera de los limites')
+        plt.text(150,60,'El HIT PONT esta fuera de los limites')
+        plt.text(175,45,'A1+A2=',color='orange')
+        plt.text(200,45,(A1+A2),color='orange')
+
     elif((A1+A2)<A):
-        plt.text(180,60,'El HIT PONT esta dentro del hoyo')
+        plt.text(150,60,'El HIT PONT esta dentro del limite')
+        plt.text(175,45,'A1+A2=',color='r')
+        plt.text(200,45,(A1+A2),color='r')
+
+    plt.text(175,80,'A=',color='b')
+    plt.text(185,80,A,color='b')
+    plt.text(175,85,'A1=',color='purple')
+    plt.text(187,85,A1,color='purple')
+    plt.text(175,90,'A2=',color='k')
+    plt.text(187,90,A2,color='k')
     
     plotPlaneLine(xg,yg,zg)
 
 
-#Pedir num control
+#Pide el hitpoint x, y o el numeoro de control
 while True:
-    enter=input('donde esta el histpoint en "x"? o Ingrese se numero de control para salir (18390045)')
-    if enter=='18390045':
+    enterx=input('donde esta el histpoint en "x"? o Ingrese se numero de control para salir (18390045)')
+    if enterx=='18390045':
         break
     else:
         
-        x[3]=int(enter)
-        enter=input('donde esta el histpoint en "y"? o Ingrese se numero de control para salir (18390045)')
-        if enter=='18390031':
+        x[3]=int(enterx)
+        entery=input('donde esta el histpoint en "y"? o Ingrese se numero de control para salir (18390045)')
+        if entery=='18390045':
             break
         else:
-            y[3]=int(enter)
+            y[3]=int(entery)
 
             for i in range(len(x)):
-                xg.append( x[i]+xc )
-                yg.append( y[i]+yc )
-                zg.append( z[i]+zc )
+                xg.append(x[i]+xc)
+                yg.append(y[i]+yc)
+                zg.append(z[i]+zc)
 
             plotSquareLinex(xc,yc,zc)
